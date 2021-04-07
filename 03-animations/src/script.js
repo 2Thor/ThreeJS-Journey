3080194029c02bd6429d075b1b2fde58fd83b5b0
Dethoor Clément <clement.dethoor@cpe.fr>
2021-04-07 20:45:44 +0200

    third exercice

---
+++ 03-animations/src/script.js
@@ -0,0 +1,62 @@
+import './style.css'
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+
+// Time
+//let time = Date.now() // With time technic
+
+// Clock 
+const clock = new THREE.Clock()
+
+// Animation
+const tick = () =>
+{   
+
+    /* // Time
+    const currentTime = Date.now()
+    const delTime = currentTime - time  // With time technic
+    time = currentTime */
+
+    // Clock
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update Objects
+    //mesh.rotation.y += 0.003 //* delTime // With time technic
+    mesh.rotation.y = elapsedTime * Math.PI
+
+    // Render
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
