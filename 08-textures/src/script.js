777c521d38b48e9dc344c81d7c1dbc8a6d88f06f
Dethoor Clément <clement.dethoor@cpe.fr>
2021-04-25 16:14:17 +0200

    Start textures

--- 08-textures/src/script.js
+++ 08-textures/src/script.js
@@ -1,8 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import gsap from 'gsap'
-import * as dat from 'dat.gui'
 
 /**
  * Base
@@ -10,26 +8,14 @@ import * as dat from 'dat.gui'
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Var
-// for debugging
-const parameters = {
-    color: 0xff0000,
-    spin: () =>
-    {
-        gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 })
-        
-    }
-}
-
 // Scene
 const scene = new THREE.Scene()
 
 /**
  * Object
  */
-
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: parameters.color })
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -61,7 +47,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 1
 scene.add(camera)
 
 // Controls
@@ -97,31 +85,3 @@ const tick = () =>
 }
 
 tick()
-
-/**
- * Debug
- */
-
-const gui = new dat.GUI({ closed: true, width: 400 })
-
-//gui.add(mesh.position, 'y')
-//gui.add(mesh.position, 'y', - 3, 3, 0.01)
-//gui.add(mesh.position, 'y').min(- 3).max(3).step(0.01)
-gui
-    .add(mesh.position, 'y')
-    .min(- 3)
-    .max(3)
-    .step(0.01)
-    .name('elevation')
-    
-gui.add(mesh, 'visible')
-gui.add(material, 'wireframe')
-
-gui
-    .addColor(parameters, 'color')
-    .onChange(() =>
-    {
-        material.color.set(parameters.color)
-    })
-
-gui.add(parameters, 'spin')
